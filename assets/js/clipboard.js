8dbf5eaa3e7732d2451e38d6a2eccaccabddfa7d
Andriy Balan <81969926+Andriy099@users.noreply.github.com>
2021-05-12 22:42:13 +0200

    Add files via upload

---
+++ assets/js/clipboard.js
@@ -0,0 +1,10 @@
+function copy(){
+    navigator.clipboard.writeText('mc.PhantomRealm.it')
+    swal({
+    title: 'IP COPIATO',
+    text: 'BUON GIOCO',
+    icon: 'success',
+    timer: 2000,
+    buttons: false,
+})
+}
